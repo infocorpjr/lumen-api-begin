76146c9470c4c5a375e585f0da1e39fe0fd2307d
Marcus Goldschmidt Oliveira <marcus.golds@hotmail.com>
2018-10-23 14:21:23 -0400

    Configurado acesso externo

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,9 +2,8 @@
 
 namespace App\Http\Controllers;
 
-
 use App\Model\Contact;
-use Illuminate\Http\Request\ContactStoreRequest;
+use Illuminate\Http\Request;
 
 class ContactController extends Controller
 {
@@ -17,27 +16,35 @@ class ContactController extends Controller
 
     public function show($id)
     {
-        return response()->json(Author::find($id));
+        return response()->json(Contact::find($id));
     }
 
-    public function create(ContactStoreRequest $request)
+    public function create(Request $request)
     {
-        $author = Author::create($request->all());
+        $rules = [
+            'name' => 'required|max:255',
+            'email' => 'required|max:255|email',
+            'subject' => 'required'
+        ];
 
-        return response()->json($author, 201);
+        $this->validate($request, $rules);
+
+        $Contact = Contact::create($request->all());
+
+        return response()->json($Contact, 201);
     }
 
-    public function update($id, ContactStoreRequest $request)
+    public function update($id, Request $request)
     {
-        $author = Author::findOrFail($id);
-        $author->update($request->all());
+        $Contact = Contact::findOrFail($id);
+        $Contact->update($request->all());
 
-        return response()->json($author, 200);
+        return response()->json($Contact, 200);
     }
 
     public function delete($id)
     {
-        Author::findOrFail($id)->delete();
+        Contact::findOrFail($id)->delete();
         return response('Deleted Successfully', 200);
     }
 
