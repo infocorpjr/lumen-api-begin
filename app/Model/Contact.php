b1eeba408c4517c465ff739742c5d1f5901000ec
Marcus Goldschmidt Oliveira <marcus.golds@hotmail.com>
2018-10-23 13:03:40 -0400

    Criado model

---
+++ app/Model/Contact.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Contact extends Model
+{
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'id', 'name', 'email', 'subject'
+    ];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = [];
+}
